ce9f83b3a6768ca4eabdc1a9274c75206c73c590
Alex Husarev <has1985r@gmail.com>
2022-07-02 00:09:05 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	tgClient "my/bot/clients/telegram"
+	"my/bot/consumer/event_consumer"
+	"my/bot/events/telegram"
+	"my/bot/storage/files"
+)
+
+const (
+	tgBotHost   = "api.telegram.org"
+	storagePath = "storage"
+	batchSize   = 100
+)
+
+func main() {
+
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
+
+	log.Print("service started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+	err := consumer.Start()
+	if err != nil {
+		log.Fatal("service is stopped", err)
+	}
+
+}
+
+func mustToken() string {
+	token := flag.String(
+		"tg-bot-token",
+		"",
+		"token for access to telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+	return *token
+}
